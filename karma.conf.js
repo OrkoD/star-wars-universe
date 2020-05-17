baa3c3c660c2125a3ffc2ab09fd02046fe01e4c0
OrestDrozda <orest.drozda@ibm.com>
2020-05-17 20:58:50 +0300

    set up jenkins

--- karma.conf.js
+++ karma.conf.js
@@ -15,6 +15,15 @@ module.exports = function (config) {
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
+    files: [
+      { pattern: './src/test.ts', watched: false }
+    ],
+    preprocessors: {
+      './src/test.ts': ['@angular/cli']
+    },
+    mime: {
+      'text/x-typescript': ['ts', 'tsx']
+    },
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, './coverage/star-wars-universe'),
       reports: ['html', 'lcovonly', 'text-summary'],
@@ -25,8 +34,8 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false,
+    browsers: ['PhantomJS'], /* remove chrome and replace it with PhantomJS */
+    singleRun: true, /* make it true to run test suits only one time */
     restartOnFileChange: true
   });
 };
